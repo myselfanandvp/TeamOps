3e008742d35c24a8abaadaa0896e87fdc9bf386f
myselfanandvp <mailanandvp@gmail.com>
2026-09-15 01:32:13 +0530

    change the db name

--- backend/config/env.py
+++ backend/config/env.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     secret_key: str = "dummy-secret-key-for-testing"
     debug: bool = False
-    db_name: str = "devtrack"
+    db_name: str = "teamops"
     db_user: str = "postgres"
     db_pass: str = "dummy-password"
     db_host: str = "localhost"
